e78abf261121aeb307a350b2f9c36a2832b91681
shilei <shilei@yuewen.com>
2017-04-12 15:43:52 +0800

    add oversea config

--- config/oversea.config.js
+++ config/oversea.config.js
@@ -1,4 +1,4 @@
-const NODE_ENV = process.env.NODE_ENV || 'local';
+const NODE_ENV = process.env.NODE_ENV || 'oa2';
 
 module.exports = {
   /**
@@ -7,40 +7,40 @@ module.exports = {
   apps: [
     {
       // 服务别名
-      name: 'm',
+      name: 'oversea',
       script: 'app.js',
       node_args: '--harmony',
       instances: 0,
       exec_mode: 'cluster',
       // 以下是日志输出选项
-      log_file: 'log/m.qidian.com/combined.log',
-      out_file: 'log/m.qidian.com/out.log',
-      error_file: 'log/m.qidian.com/err.log',
+      log_file: '/data/logs/oa2www.webnovel.com/combined.log',
+      out_file: '/data/logs/oa2www.webnovel.com/out.log',
+      error_file: '/data/logs/oa2www.webnovel.com/err.log',
       merge_logs: true,
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       // 以下是站点配置
       env: {
-        NODE_SITE: 'm', // NODE服务项目别名
+        NODE_SITE: 'oversea', // NODE服务项目别名
         NODE_ENV: NODE_ENV, // 当前Node服务环境
-        port: 10500, // 服务端口
+        port: 10501, // 服务端口
         CONFIG_FILE: 'off', // 设置为 on 时,兼容旧有配置文件形式
         // 站点配置
         config: JSON.stringify({
           // 是否开启taf上报
           stat: false,
           // 是否开启L5 taf平台适用
-          l5_on: false, 
+          l5_on: true, 
           // 后端是否采用https协议
           cgi_ssl_on: false,
 
           // 项目配置文件夹地址
-          path: '/Users/shilei/qidian-m/.cache/config',
+          path: '/data/website/en.qidian.com/views/node-config',
           // 配置文件名,默认为 server.js
           server_conf_file: 'server',
           // extends文件或文件夹名，如果是文件夹默认加载文件夹内的index，没有index的话加载loader
           extends_file: 'extends',
           // 是否开启非0自定义handler
-          custom_handle_on: true,
+          custom_handle_on: false,
           // 非0自定义handler文件路径
           custom_handle_file: '',
           // 是否开启错误重定向，开启则如果发生错误定向到统一错误路径，关闭会渲染模板目录下的error.html
@@ -49,7 +49,7 @@ module.exports = {
           error_redirct_path: '',
 
           // 动态路由映射文件或文件夹名,默认为 routermap，如果是文件夹默认加载文件夹内的index
-          routermap_file: 'routes', 
+          routermap_file: 'dynamic_routermap.js', 
           // 是否开启 inline-ejs,提供忽略 <script type="text/ejs-template"></script> 功能
           inline_ejs: true,
           // 开启 inline-ejs后，可自定义 script 标签的 type 属性，默认为 text/ejs-template
@@ -60,9 +60,9 @@ module.exports = {
           force_render: false, 
 
           // 是否开启静态化服务
-          static_server_on: true,
+          static_server_on: false,
           // 静态化路由配合文件,默认为 static_routermap
-          static_routermap_file: 'static_routermap',
+          static_routermap_file: 'static_routermap.js',
           // 是否开启静态化 html 压缩
           compressHTML: true,
           // 静态化服务原有后端接口，后端post所有页面数据，不使用此静态化接口改为空字符串即可
