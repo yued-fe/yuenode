ece9e7efdf65c7759fa003d681189c02d9676635
shilei <shilei@yuewen.com>
2017-04-19 16:54:43 +0800

    add sh

--- config/m.config.js
+++ config/m.config.js
@@ -13,9 +13,9 @@ module.exports = {
       instances: 0,
       exec_mode: 'cluster',
       // 以下是日志输出选项
-      log_file: 'log/m.qidian.com/combined.log',
-      out_file: 'log/m.qidian.com/out.log',
-      error_file: 'log/m.qidian.com/err.log',
+      log_file: 'logs/m.qidian.com/combined.log',
+      out_file: 'logs/m.qidian.com/out.log',
+      error_file: 'logs/m.qidian.com/err.log',
       merge_logs: true,
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       // 以下是站点配置
